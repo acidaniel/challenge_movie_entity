a4dde8a6f30acdb72e611b814f8447ae44462e83
Daniel Serna <acidaniel@gmail.com>
2023-04-11 23:54:00 -0600

    Adding validation js and css formatting to admin forms.

--- js/form_movie_validate.js
+++ js/form_movie_validate.js
@@ -1,11 +1,18 @@
-function submit_valid() {
-    console.log('test');
-}
+// Adding custom pure JavaScript tp validate date field.
+document.querySelector('#movie-add-form').addEventListener('submit', function(event) {
+    // Getting Date Value from field.
+    var dateField = document.querySelector('#edit-release-date-0-value-date');
+    var dateValue = dateField.value;
+    // Getting current Date.
+    var currentDate = new Date().getTime();
+    // Converting selected Date to time.
+    var selectedDate = new Date(dateValue).getTime();
+    // Compare if selected date is greater than now.
+    if (selectedDate > currentDate) {
+      // Disable submit.
+      event.preventDefault();
+      // Adding custom class to date field.
+      dateField.classList.add('future_date');
+    }
 
-(function ($) {
-    $(document).ready(
-        function () {
-            console.log(submit_valid());
-        }
-    )
-})
+});
